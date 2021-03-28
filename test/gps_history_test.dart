098dff2fb4a31c7c53eef134e6e832ab77dd9527
anxix <>
2021-03-28 19:32:27 +0100

    Added tests for measurement class

--- test/gps_history_test.dart
+++ test/gps_history_test.dart
@@ -18,4 +18,28 @@ void main() {
     test('Check longitude', () => expect(point.longitude, 20));
     test('Check altitude', () => expect(point.altitude, 30));
   });
+
+  group('Test GpsMeasurement', () {
+    GpsMeasurement m =
+        GpsMeasurement(DateTime.utc(2020), 10, 20, 30, 40, 50, 60, 70);
+
+    test('Check time', () => expect(m.time, DateTime.utc(2020)));
+    test('Check latitude', () => expect(m.latitude, 10));
+    test('Check longitude', () => expect(m.longitude, 20));
+    test('Check altitude', () => expect(m.altitude, 30));
+    test('Check accuracy', () => expect(m.accuracy, 40));
+    test('Check heading', () => expect(m.heading, 50));
+    test('Check speed', () => expect(m.speed, 60));
+    test('Check speedAccuracy', () => expect(m.speedAccuracy, 70));
+  });
+
+  group('Test GpsMeasurement nulls', () {
+    GpsMeasurement m =
+        GpsMeasurement(DateTime.utc(2020), 10, 20, 30, null, null, null, null);
+
+    test('Check accuracy', () => expect(m.accuracy, null));
+    test('Check heading', () => expect(m.heading, null));
+    test('Check speed', () => expect(m.speed, null));
+    test('Check speedAccuracy', () => expect(m.speedAccuracy, null));
+  });
 }
