1154ce028ed5d275e3eaa429daadb52065cf688b
anxix <>
2021-03-28 12:55:04 +0100

    Enabled null safety and fixed generated code accordingly.

--- test/gps_history_test.dart
+++ test/gps_history_test.dart
@@ -1,16 +1,24 @@
+/*
+ * Copyright (c) 
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import 'package:gps_history/gps_history.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    Awesome awesome;
+    Awesome? awesome;
 
     setUp(() {
       awesome = Awesome();
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      expect(awesome?.isAwesome, isTrue);
     });
   });
 }
