40b982e9982512411e506d79581e50f8ecbc9bca
anxix <>
2021-03-28 11:20:53 +0100

    first commit

---
+++ example/gps_history_example.dart
@@ -0,0 +1,6 @@
+import 'package:gps_history/gps_history.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/gps_history.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library gps_history;
+
+export 'src/gps_history_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
