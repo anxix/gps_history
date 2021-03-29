839c8eaf4cc53fa2314369afb895f6e8afb24b75
anxix <>
2021-03-29 22:07:08 +0100

    Refactoring the test code

--- test/base_test.dart
+++ test/base_test.dart
@@ -8,84 +8,56 @@
 import 'package:test/test.dart';
 import 'package:gps_history/gps_history.dart';
 
+testUnequalPoints(String description, GpsPoint p0, GpsPoint p1) {
+  test('Check inequality by $description', () => expect(p0 == p1, false));
+}
+
+testBasicPoint(GpsPoint p) {
+  test('Check time', () => expect(p.time, DateTime.utc(2020)));
+  test('Check latitude', () => expect(p.latitude, 10));
+  test('Check longitude', () => expect(p.longitude, 20));
+  test('Check altitude', () => expect(p.altitude, 30));
+  test('Check equality of same object', () => expect(p, p));
+}
+
 void main() {
   group('Test GpsPoint', () {
-    GpsPoint point = GpsPoint(DateTime.utc(2020), 10, 20, 30);
+    GpsPoint p = GpsPoint(DateTime.utc(2020), 10, 20, 30);
 
-    test('Check time', () => expect(point.time, DateTime.utc(2020)));
-    test('Check latitude', () => expect(point.latitude, 10));
-    test('Check longitude', () => expect(point.longitude, 20));
-    test('Check altitude', () => expect(point.altitude, 30));
-    test('Check equality of same object', () => expect(point, point));
+    testBasicPoint(p);
     test('Check equality of different object with same values',
-        () => expect(point, GpsPoint(DateTime.utc(2020), 10, 20, 30)));
-    test('Check inequality by date',
-        () => expect(point == GpsPoint(DateTime.utc(2021), 10, 20, 30), false));
-    test('Check inequality by latitude',
-        () => expect(point == GpsPoint(DateTime.utc(2020), 11, 20, 30), false));
-    test('Check inequality by longitude',
-        () => expect(point == GpsPoint(DateTime.utc(2020), 10, 21, 30), false));
-    test('Check inequality by altitude',
-        () => expect(point == GpsPoint(DateTime.utc(2020), 10, 20, 31), false));
+        () => expect(p, GpsPoint(DateTime.utc(2020), 10, 20, 30)));
+    testUnequalPoints('date', p, GpsPoint(DateTime.utc(2021), 10, 20, 30));
+    testUnequalPoints('latitude', p, GpsPoint(DateTime.utc(2020), 11, 20, 30));
+    testUnequalPoints('longitude', p, GpsPoint(DateTime.utc(2020), 10, 21, 30));
+    testUnequalPoints('altitude', p, GpsPoint(DateTime.utc(2020), 10, 20, 31));
   });
 
   group('Test GpsMeasurement', () {
     GpsMeasurement m =
         GpsMeasurement(DateTime.utc(2020), 10, 20, 30, 40, 50, 60, 70);
 
-    test('Check time', () => expect(m.time, DateTime.utc(2020)));
-    test('Check latitude', () => expect(m.latitude, 10));
-    test('Check longitude', () => expect(m.longitude, 20));
-    test('Check altitude', () => expect(m.altitude, 30));
-    test('Check accuracy', () => expect(m.accuracy, 40));
-    test('Check heading', () => expect(m.heading, 50));
-    test('Check speed', () => expect(m.speed, 60));
-    test('Check speedAccuracy', () => expect(m.speedAccuracy, 70));
-    test('Check equality of same object', () => expect(m, m));
+    testBasicPoint(m);
     test(
         'Check equality of different object with same values',
         () => expect(
             m, GpsMeasurement(DateTime.utc(2020), 10, 20, 30, 40, 50, 60, 70)));
-    test(
-        'Check inequality by date',
-        () => expect(
-            m == GpsMeasurement(DateTime.utc(2021), 10, 20, 30, 40, 50, 60, 70),
-            false));
-    test(
-        'Check inequality by latitude',
-        () => expect(
-            m == GpsMeasurement(DateTime.utc(2020), 11, 20, 30, 40, 50, 60, 70),
-            false));
-    test(
-        'Check inequality by longitude',
-        () => expect(
-            m == GpsMeasurement(DateTime.utc(2020), 10, 21, 30, 40, 50, 60, 70),
-            false));
-    test(
-        'Check inequality by altitude',
-        () => expect(
-            m == GpsMeasurement(DateTime.utc(2020), 10, 20, 31, 40, 50, 60, 70),
-            false));
-    test(
-        'Check inequality by accuracy',
-        () => expect(
-            m == GpsMeasurement(DateTime.utc(2020), 10, 20, 30, 41, 50, 60, 70),
-            false));
-    test(
-        'Check inequality by heading',
-        () => expect(
-            m == GpsMeasurement(DateTime.utc(2020), 10, 20, 30, 40, 51, 60, 70),
-            false));
-    test(
-        'Check inequality by speed',
-        () => expect(
-            m == GpsMeasurement(DateTime.utc(2020), 10, 20, 30, 40, 50, 61, 70),
-            false));
-    test(
-        'Check inequality by speedAccuracy',
-        () => expect(
-            m == GpsMeasurement(DateTime.utc(2020), 10, 20, 30, 40, 50, 60, 71),
-            false));
+    testUnequalPoints('date', m,
+        GpsMeasurement(DateTime.utc(2021), 10, 20, 30, 40, 50, 60, 70));
+    testUnequalPoints('latitude', m,
+        GpsMeasurement(DateTime.utc(2020), 11, 20, 30, 40, 50, 60, 70));
+    testUnequalPoints('longitude', m,
+        GpsMeasurement(DateTime.utc(2020), 10, 21, 30, 40, 50, 60, 70));
+    testUnequalPoints('altitude', m,
+        GpsMeasurement(DateTime.utc(2020), 10, 20, 31, 40, 50, 60, 70));
+    testUnequalPoints('accuracy', m,
+        GpsMeasurement(DateTime.utc(2020), 10, 20, 30, 41, 50, 60, 70));
+    testUnequalPoints('heading', m,
+        GpsMeasurement(DateTime.utc(2020), 10, 20, 30, 40, 51, 60, 70));
+    testUnequalPoints('speed', m,
+        GpsMeasurement(DateTime.utc(2020), 10, 20, 30, 40, 50, 61, 70));
+    testUnequalPoints('speedAccuracy', m,
+        GpsMeasurement(DateTime.utc(2020), 10, 20, 30, 40, 50, 60, 71));
   });
 
   group('Test GpsMeasurement nulls', () {
