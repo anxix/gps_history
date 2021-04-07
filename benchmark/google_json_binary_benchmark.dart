02a281f12a3e46367b65df866d476944f421063c
anxix <51137713+anxix@users.noreply.github.com>
2021-04-07 20:26:08 +0100

    Added benchmark code for the Google JSON history parser

---
+++ benchmark/google_json_binary_benchmark.dart
@@ -0,0 +1,66 @@
+/* Copyright (c)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+import 'package:gps_history/gps_history.dart';
+import 'package:gps_history/gps_history_convert.dart';
+
+/// Try out the performance of the string-based JSON parser versus the
+/// byte-based one. Can also be used to check that their results are the same.
+void main() async {
+  var method = 1; // use 0 for string-based, 1 for integer-based
+
+  final file =
+      File('/home/me/src/gps_history/example/Locatiegeschiedenis.json');
+  final s = Stopwatch();
+  final gpsPoints = GpcCompactGpsPoint();
+
+  var fileStream = file.openRead();
+
+  s.start();
+
+  Stream<GpsPoint> points = method == 0
+      ? fileStream
+          .transform(Utf8Decoder(allowMalformed: true))
+          .transform(GoogleJsonHistoryDecoder())
+      : fileStream.transform(GoogleJsonHistoryDecoderBinary());
+
+  await for (var p in points) {
+    gpsPoints.add(p);
+  }
+
+  s.stop();
+  final dt = s.elapsedMilliseconds / 1000;
+  print(
+      'Read ${gpsPoints.length} in $dt s: ${gpsPoints.length / 1000000 / dt} Mpoints/s or ${dt / (gpsPoints.length / 1000000)} s/Mpoint');
+
+  var diffs = <int>[];
+  var sumdiffs = 0;
+  var prevp = null;
+  var mindiff = 100000000;
+  var maxdiff = 0;
+
+  for (var p in gpsPoints) {
+    if (prevp != null) {
+      final diff = p.time.difference(prevp.time).inSeconds;
+      if (diff > 0) {
+        mindiff = min(diff, mindiff);
+      }
+      maxdiff = max(diff, maxdiff);
+      sumdiffs += diff;
+      diffs.add(diff);
+    }
+    prevp = p;
+  }
+
+  diffs.sort();
+
+  print(
+      'maxdiff=$maxdiff, mindiff=$mindiff, avgdiff=${sumdiffs / diffs.length}, mediandiff=${diffs[diffs.length ~/ 2]}');
+}
