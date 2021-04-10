50326fc43557c685cbf290b5e87b54b46f99c222
anxix <51137713+anxix@users.noreply.github.com>
2021-04-10 20:51:59 +0100

    Minor tweaks

--- tool/google_json_unique_keys.py
+++ tool/google_json_unique_keys.py
@@ -1,15 +1,16 @@
 """
 Script to identify all unique keys in a Google location history JSON export.
 """
-import re;
+import re
 
-r = re.compile('\w+');
-unique_names = set();
-for line in open('Locatiegeschiedenis.json', 'r').readlines():
-    m = r.findall(line);
+filename = r'Locatiegeschiedenis.json'
+
+r = re.compile('\w+')
+unique_names = set()
+for line in open(filename, 'r').readlines():
+    m = r.findall(line)
     if len(m) > 0:
         unique_names.add(m[0])
 
 for name in sorted(unique_names):
     print('{} : {}'.format(name, len(name)))
-
