2f688870a084ee84d982d3086e55b72f1e5933d9
anxix <51137713+anxix@users.noreply.github.com>
2021-04-10 18:30:17 +0100

    Convert PointParserString unit tests to PointParser.

--- test/src/convert/google_json_test.dart
+++ test/src/convert/google_json_test.dart
@@ -11,131 +11,136 @@ import 'package:gps_history/gps_history_convert.dart';
 
 final oneDay = 24 * 3600 * 1000; // one day in milliseconds
 
-/// Tests the PointParser against the specified sequence of [lines], ensuring
-/// that it returns the correct response after each line ([expectedPoints]).
+/// Converts [string] to a list of integers being the code units of its
+/// characters.
+List<int> stringToIntList(String string) {
+  final result = List<int>.filled(string.length, 0);
+
+  // Test as single chunk.
+  for (var i = 0; i < string.length; i++) {
+    result[i] = string.codeUnitAt(i);
+  }
+
+  return result;
+}
+
+/// Tests the PointParser against the specified sequence of [chunks], ensuring
+/// that it returns the correct response after parsing ([expectedPoints]).
 void _testPointParser(
-    String testName, List<String> lines, List<GpsPoint?> expectedPoints) {
-  // Checks for the given point that it matche sthe first item in
-  // [expectedPoints]. If it doesn't, the test is failed.
-  void checkParserResult(GpsPoint? point) {
-    if (expectedPoints.isEmpty) {
-      fail('Returned more points than expected');
+    String testName, List<List<int>> chunks, List<GpsPoint?> expectedPoints) {
+  void checkParserResult(List<GpsPoint> foundPoints) {
+    expect(foundPoints.length, expectedPoints.length,
+        reason: 'Incorrect number of parsed points.');
+
+    for (var pointNr = 0; pointNr < foundPoints.length; pointNr++) {
+      expect(foundPoints[pointNr], expectedPoints[pointNr],
+          reason: 'Wrong point at position $pointNr');
     }
-
-    expect(point, expectedPoints[0], reason: 'Incorrect result after parsing');
-
-    expectedPoints.removeAt(0);
   }
 
   test(testName, () {
-    final parser = PointParserString();
+    final foundPoints = <GpsPoint>[];
+    final Function(GpsPoint point) pointsCollector = (GpsPoint point) {
+      foundPoints.add(point);
+    };
+    final parser = PointParser(null, null, pointsCollector);
 
-    for (var line in lines) {
-      final point = parser.parseUpdate(line);
-
-      checkParserResult(point);
+    for (var chunk in chunks) {
+      parser.parseUpdate(chunk, 0, chunk.length);
     }
-
-    // At the end there may still be a valid point state in the parser, so
-    // check for that.
-    final point = parser.toGpsPointAndReset();
-    if (point != null) {
-      checkParserResult(point);
-    }
-
-    // After we extracted the last bit of information, the parser must not
-    // return any more points.
-    expect(parser.toGpsPointAndReset(), null,
-        reason: 'Parser returned more than one final point.');
-
+    // Force outputting of any final point the parser is unsure of whether it's
+    // been fully parsed or not.
+    parser.toGpsPointAndReset();
     expect(parser.isAllNull, true,
         reason: 'Parser state not all null after extracting it last state.');
 
-    if (expectedPoints.isNotEmpty) {
-      fail('Not all expected points found!');
-    }
+    checkParserResult(foundPoints);
+
+    // After we extracted the last bit of information, the parser must not
+    // return any more points.
+    final oldNrFoundPoints = foundPoints.length;
+    parser.toGpsPointAndReset();
+    expect(foundPoints.length, oldNrFoundPoints,
+        reason: 'Parser returned more than one final point.');
   });
 }
 
-/// Simplified version of [_testPointParser] that expects nulls for every
-/// line in [lines] and only tests the final parser state at the end against
-/// the [expectedPoint].
-void _testPointParserAllNullsAndLastState(
-    String testName, List<String> lines, GpsPoint? expectedPoint) {
-  var expectedPoints = <GpsPoint?>[];
-
-  for (var _ in lines) {
-    expectedPoints.add(null);
+/// Tests the PointParser against the specified sequence of [strings], ensuring
+/// that it returns the correct response after parsing ([expectedPoints]).
+void _testPointParserStrings(
+    String testName, List<String> strings, List<GpsPoint?> expectedPoints) {
+  final chunks = List<List<int>>.filled(0, [], growable: true);
+  for (var string in strings) {
+    chunks.add(stringToIntList(string));
   }
 
-  expectedPoints.add(expectedPoint);
-  _testPointParser(testName, lines, expectedPoints);
+  _testPointParser(testName, chunks, expectedPoints);
 }
 
+/// Test various cases for the [PointParser] class.
 void testPointParser() {
   // Test the empty cases.
-  _testPointParser('Nothing', [], []);
-  _testPointParser('Empty string', [''], [null]);
+  _testPointParserStrings('Nothing', [], []);
+  _testPointParserStrings('Empty string', [''], [null]);
 
   // Test arbitrary junk data.
-  _testPointParser(
-      'Arbitrary strings',
-      ['wnvoiuvh', '"aiuwhe"', '"niniwuev" : "nioj"', '"jnj9aoiue": 3298'],
-      [null, null, null, null]);
+  _testPointParserStrings('Arbitrary strings',
+      ['wnvoiuvh\n"aiuwhe"\n"niniwuev" : "nioj"\n"jnj9aoiue": 3298'], []);
 
   // Test simple one-point defintion.
-  _testPointParser(
-      'Parse invalid point in standard order',
-      ['"timestampMs":0,', '"latitudeE7" :-,', '"longitudeE7": 2,'],
-      [null, null, null]);
-  _testPointParserAllNullsAndLastState(
+  _testPointParserStrings('Parse invalid point in standard order',
+      ['"timestampMs":0,\n"latitudeE7" :-,\n"longitudeE7": 2,'], []);
+  _testPointParserStrings(
       'Parse single point in standard order',
-      ['"timestampMs":0,', '"latitudeE7" :1,', '"longitudeE7": 2'],
-      GpsPoint(DateTime.utc(1970), 1.0E-7, 2.0E-7, null));
-  _testPointParserAllNullsAndLastState(
-      'Test parsing when last character is part of number',
-      ['"timestampMs":0,', '"latitudeE7" :13', '"longitudeE7": 20'],
-      GpsPoint(DateTime.utc(1970), 1.3E-6, 2.0E-6, null));
-  _testPointParserAllNullsAndLastState(
+      ['"timestampMs":0,\n"latitudeE7" :1, "longitudeE7": 2'],
+      [GpsPoint(DateTime.utc(1970), 1.0E-7, 2.0E-7, null)]);
+  _testPointParser('Test parsing when last character is part of number', [
+    // Use raw stringToIntList instead of calling testPointParserStrings
+    // because we want to prevent any automatic string processing that
+    // the testPointParserStrings may do from affecting the shape of the
+    // string. In particular we don't want any space/newline to be added
+    // at the end, the last character must be a digit.
+    stringToIntList('"timestampMs":0,\n"latitudeE7" :13\n"longitudeE7": 20')
+  ], [
+    GpsPoint(DateTime.utc(1970), 1.3E-6, 2.0E-6, null)
+  ]);
+  _testPointParserStrings(
       'Parse single point in nonstandard order',
       ['"latitudeE7" : \'1\',', '"timestampMs" : "0",', '"longitudeE7" : "2"'],
-      GpsPoint(DateTime.utc(1970), 1.0E-7, 2.0E-7, null));
-  _testPointParserAllNullsAndLastState(
-      'Parse single point with fluff in between',
-      [
-        '"timestampMs" : 0,',
-        '"latitudeE7" : 1,',
-        '"x" : 8',
-        '"longitudeE7" : 2,'
-      ],
-      GpsPoint(DateTime.utc(1970), 1.0E-7, 2.0E-7, null));
+      [GpsPoint(DateTime.utc(1970), 1.0E-7, 2.0E-7, null)]);
+  _testPointParserStrings('Parse single point with fluff in between', [
+    '"timestampMs" : 0,',
+    '"latitudeE7" : 1,',
+    '"x" : 8',
+    '"longitudeE7" : 2,'
+  ], [
+    GpsPoint(DateTime.utc(1970), 1.0E-7, 2.0E-7, null)
+  ]);
 
   // Test resetting of internal state after incomplete initial point state.
-  _testPointParserAllNullsAndLastState(
-      'Parse single point after incomplete point',
-      [
-        '"timestampMs" : 99999,',
-        '"latitudeE7" : 1,',
-        '"timestampMs" : $oneDay,', // this should lead to the above two being discarded
-        '"latitudeE7" : 5,',
-        '"longitudeE7" : 6,',
-        '"altitude" : 8,'
-      ],
-      GpsPoint(DateTime.utc(1970, 1, 2), 5.0E-7, 6.0E-7, 8.0));
+  _testPointParserStrings('Parse single point after incomplete point', [
+    '"timestampMs" : 99999,',
+    '"latitudeE7" : 1,',
+    '"timestampMs" : $oneDay,', // this should lead to the above two being discarded
+    '"latitudeE7" : 5,',
+    '"longitudeE7" : 6,',
+    '"altitude" : 8,'
+  ], [
+    GpsPoint(DateTime.utc(1970, 1, 2), 5.0E-7, 6.0E-7, 8.0)
+  ]);
 
   // Test negative values
-  _testPointParserAllNullsAndLastState(
-      'Parse negative values',
-      [
-        '"timestampMs" : -$oneDay,', // this should lead to the above two being discarded
-        '"latitudeE7" : -5,',
-        '"longitudeE7" : -6,',
-        '"altitude" : -80,'
-      ],
-      GpsPoint(DateTime.utc(1969, 12, 31), -5.0E-7, -6.0E-7, -80.0));
+  _testPointParserStrings('Parse negative values', [
+    '"timestampMs" : -$oneDay,', // this should lead to the above two being discarded
+    '"latitudeE7" : -5,',
+    '"longitudeE7" : -6,',
+    '"altitude" : -80,'
+  ], [
+    GpsPoint(DateTime.utc(1969, 12, 31), -5.0E-7, -6.0E-7, -80.0)
+  ]);
 
   // Test parsing of multiple points.
-  _testPointParser('Parse two consecutive points', [
+  _testPointParserStrings('Parse two consecutive points', [
     '"timestampMs" : 0,',
     '"latitudeE7" : 1,',
     '"longitudeE7" : 2,',
@@ -143,42 +148,35 @@ void testPointParser() {
     '"latitudeE7" : 5,',
     '"longitudeE7" : 6,'
   ], [
-    null,
-    null,
-    null, // Here it doesn't yet know the point is fully defined (finds that next).
     GpsPoint(DateTime.utc(1970), 1.0E-7, 2.0E-7, null),
-    null,
-    null,
     GpsPoint(DateTime.utc(1970, 1, 2), 5.0E-7, 6.0E-7, null)
   ]);
 
   // Test parsing to [GpsMeasurement].
-  _testPointParserAllNullsAndLastState(
-      'Parse to GpsMeasurement',
-      [
-        '"timestampMs" : 0,',
-        '"latitudeE7" : 1,',
-        '"longitudeE7" : 2,',
-        '"accuracy" : 12,',
-      ],
-      GpsMeasurement(
-          DateTime.utc(1970), 1.0E-7, 2.0E-7, null, 12, null, null, null));
+  _testPointParserStrings('Parse to GpsMeasurement', [
+    '"timestampMs" : 0,',
+    '"latitudeE7" : 1,',
+    '"longitudeE7" : 2,',
+    '"accuracy" : 12,',
+  ], [
+    GpsMeasurement(
+        DateTime.utc(1970), 1.0E-7, 2.0E-7, null, 12, null, null, null)
+  ]);
 
   // Test parsing with some real data.
-  _testPointParserAllNullsAndLastState(
-      'Parse real data',
-      [
-        '}, {',
-        '"timestampMs" : "1616789690748",',
-        '"latitudeE7" : 371395513,',
-        '"longitudeE7" : -79376766,',
-        '"accuracy" : 20,',
-        '"altitude" : 402,',
-        '"verticalAccuracy" : 3',
+  _testPointParserStrings('Parse real data', [
+    '}, {\n'
+        '"timestampMs" : "1616789690748",\n'
+        '"latitudeE7" : 371395513,\n'
+        '"longitudeE7" : -79376766,\n'
+        '"accuracy" : 20,\n'
+        '"altitude" : 402,\n'
+        '"verticalAccuracy" : 3\n'
         '}, {'
-      ],
-      GpsMeasurement(DateTime.utc(2021, 3, 26, 20, 14, 50, 748), 37.1395513,
-          -7.9376766, 402, 20, null, null, null));
+  ], [
+    GpsMeasurement(DateTime.utc(2021, 3, 26, 20, 14, 50, 748), 37.1395513,
+        -7.9376766, 402, 20, null, null, null)
+  ]);
 }
 
 /// Runs a conversion test of the specified [jsonByteChunks] and checks if they
@@ -198,14 +196,9 @@ void testChunkedJsonToGps(String testName, List<List<int>> jsonByteChunks,
 /// specified [json], by setting [testAllChunkingPairs] to true.
 void testJsonToGps(String testName, String json, List<GpsPoint> expectedPoints,
     [bool testAllChunkingPairs = false]) {
-  final stringAsIntList = List<int>.filled(json.length, 0);
+  final stringAsIntList = stringToIntList(json);
 
-  // Test as single chunk.
-  for (var i = 0; i < json.length; i++) {
-    stringAsIntList[i] = json.codeUnitAt(i);
-  }
   final chunkedList = [stringAsIntList];
-
   testChunkedJsonToGps(testName, chunkedList, expectedPoints);
 
   // Test every possible split into two chunks for the specified JSON. All of
