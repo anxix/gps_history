0542c6590ce933065ecedf8c9312e8f3dcf1db92
anxix <51137713+anxix@users.noreply.github.com>
2021-04-10 22:56:10 +0100

    Made the "press to wait" actually wait

--- tool/prep_release.sh
+++ tool/prep_release.sh
@@ -6,7 +6,7 @@ rm -rf doc/api
 dartdoc
 dart pub publish --dry-run
 read -p 'Release name (e.g. 1.0.2): ' relname
-echo Preparing to release $relname on GitHub. Press ENTER to continue.
+read -p Preparing to release $relname on GitHub. Press ENTER to continue.
 gh release create $relname -F CHANGELOG.md
-echo Preparing to publish $relname on pub.dev. Press ENTER to continue.
+read -p Preparing to publish $relname on pub.dev. Press ENTER to continue.
 dart pub publish
