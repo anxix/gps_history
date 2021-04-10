4e60983d383260d062037e3d3622d9bebbc11c36
anxix <51137713+anxix@users.noreply.github.com>
2021-04-10 19:10:09 +0100

    Updated release script to also do the github release.

--- tool/prep_release.sh
+++ tool/prep_release.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
+# Requires: git, dart, dartdoc, github CLI
 git pull
 rm -rf doc/api
 dartdoc
 dart pub publish --dry-run
+read -p 'Release name (e.g. 1.0.2): ' relname
+echo Preparing to release $relname on GitHub. Press ENTER to continue.
+gh release create $relname -F CHANGELOG.md
+echo Preparing to publish $relname on pub.dev. Press ENTER to continue.
 dart pub publish
