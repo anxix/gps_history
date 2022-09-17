6281609e37870402d90d9e4c2b56c6ca6b3b6f63
anxix <51137713+anxix@users.noreply.github.com>
2022-09-17 22:06:21 +0200

    Added benchmark for the add() method, since we're trying to see how expensive the sorting-related checks are.

---
+++ benchmark/point_addition.dart
@@ -0,0 +1,25 @@
+/* Copyright (c)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import 'package:gps_history/gps_history.dart';
+
+void main() async {
+  final points = GpcCompactGpsPoint();
+  points.capacity = 10000000;
+
+  final point = GpsPoint(DateTime.now().toUtc(), 0, 0, 0);
+  final startTime = DateTime.now();
+
+  for (var i = 0; i < points.capacity; i++) {
+    points.add(point);
+  }
+
+  final endTime = DateTime.now();
+  final deltaTime = endTime.difference(startTime);
+
+  print('Added ${points.length} in ${deltaTime.inMilliseconds} ms');
+}
