ec4b35366d6eeb0b2115ea422bcd9e5bbb184be2
anxix <51137713+anxix@users.noreply.github.com>
2022-09-20 00:45:08 +0200

    Enriched the point_addition benchmark to better examine the effects of sorting and different sources.

--- benchmark/point_addition.dart
+++ benchmark/point_addition.dart
@@ -7,19 +7,65 @@
 
 import 'package:gps_history/gps_history.dart';
 
-void main() async {
-  final points = GpcCompactGpsPoint();
-  points.capacity = 10000000;
+// Configure the tests to be executed. Note that CPUs may start throttling
+// if the test load is too high, leading to unreliable and strange results.
+const nrPoints = 5000000;
+const nrRunsPerTest = 5;
+const runFromBasicList = true;
+const runFromGpc = true;
 
-  final point = GpsPoint.allZero.copyWith(time: DateTime.now().toUtc());
-  final startTime = DateTime.now();
+Future<void> runPerformanceTest(
+    Iterable<GpsPoint> source, SortingEnforcement sortingEnforcement) async {
+  print(
+      'START performance test adding from ${source.runtimeType} with enforcement $sortingEnforcement');
 
-  for (var i = 0; i < points.capacity; i++) {
-    points.add(point);
+  var totalTime = Duration();
+  for (var runNr = 0; runNr < nrRunsPerTest; runNr++) {
+    final target = GpcCompactGpsPoint();
+    target.capacity = source.length;
+    target.sortingEnforcement = sortingEnforcement;
+
+    final startTime = DateTime.now();
+
+    target.addAll(source);
+    // for (final p in source) {
+    //   target.add(p);
+    // }
+
+    final endTime = DateTime.now();
+    final deltaTime = endTime.difference(startTime);
+    print(
+        '    Run $runNr: added ${target.length} in ${deltaTime.inMilliseconds} ms');
+
+    totalTime = totalTime + deltaTime;
   }
 
-  final endTime = DateTime.now();
-  final deltaTime = endTime.difference(startTime);
+  print('END. Average: ${totalTime.inMilliseconds ~/ nrRunsPerTest} ms/run.');
+}
 
-  print('Added ${points.length} in ${deltaTime.inMilliseconds} ms');
+void main() async {
+  print('Generating $nrPoints source points...');
+  final source = <GpsPoint>[];
+  final gpcSource = GpcCompactGpsPoint()..capacity = source.length;
+  for (var i = 0; i < nrPoints; i++) {
+    final p = GpsPoint(
+        time: DateTime.fromMillisecondsSinceEpoch(i * 1000),
+        latitude: i.toDouble(),
+        longitude: i.toDouble());
+    if (runFromBasicList) source.add(p);
+    if (runFromGpc) gpcSource.add(p);
+  }
+  print('Done.');
+
+  if (runFromBasicList) {
+    await runPerformanceTest(source, SortingEnforcement.throwIfWrongItems);
+    await runPerformanceTest(source, SortingEnforcement.skipWrongItems);
+    await runPerformanceTest(source, SortingEnforcement.notRequired);
+  }
+
+  if (runFromGpc) {
+    await runPerformanceTest(gpcSource, SortingEnforcement.throwIfWrongItems);
+    await runPerformanceTest(gpcSource, SortingEnforcement.skipWrongItems);
+    await runPerformanceTest(gpcSource, SortingEnforcement.notRequired);
+  }
 }
