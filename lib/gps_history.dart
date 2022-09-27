c16ceb0905b88be5a2c3b5ee421260c7af837d01
anxix <51137713+anxix@users.noreply.github.com>
2022-09-27 22:11:43 +0200

    Expose distance in the package, to make it possible to use it.

--- lib/gps_history.dart
+++ lib/gps_history.dart
@@ -12,6 +12,7 @@ library gps_history;
 
 export 'src/base.dart';
 export 'src/base_containers.dart';
+export 'src/distance.dart';
 export 'src/time.dart';
 export 'src/gpc_efficient.dart';
 export 'src/gpc_list_based.dart';
