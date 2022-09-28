a285ec6a57401e97d35c3f5fb99d3cc97cd02f11
anxix <51137713+anxix@users.noreply.github.com>
2022-09-28 20:08:57 +0200

    Changed sorting of exports.

--- lib/gps_history.dart
+++ lib/gps_history.dart
@@ -12,8 +12,8 @@ library gps_history;
 
 export 'src/base.dart';
 export 'src/base_containers.dart';
-export 'src/utils/distance.dart';
-export 'src/utils/time.dart';
 export 'src/gpc_efficient.dart';
 export 'src/gpc_list_based.dart';
 export 'src/gpv_querier.dart';
+export 'src/utils/distance.dart';
+export 'src/utils/time.dart';
