205d5d76e948a853fdb247c61219d71c2e13b9ad
anxix <51137713+anxix@users.noreply.github.com>
2022-09-30 18:17:20 +0200

    Working on collections

--- lib/src/queries/queries.dart
+++ lib/src/queries/queries.dart
@@ -1,4 +1,5 @@
-/// Queries allow seleting particular subdata from containers of GPS point data.
+/// Queries allow seleting particular subdata from collsection of GPS point
+/// data.
 ///
 /// Queries:
 ///
@@ -67,39 +68,86 @@
 
 import 'package:gps_history/gps_history.dart';
 
+/// Abstract ancestor class for query results.
+///
+/// The goal is to be able to send queries to a separate isolate and get the
+/// results back efficiently. This requires the [QueryResult] children to store
+/// internally a representation that is cheap to transfer over an isolate port.
+/// In particular for transferring point collections, the efficient collections
+/// perform very well, but the list-based collection is very slow.
 abstract class QueryResult {}
 
-/// Abstract generic ancestor class for queries parametrized for container type
+/// Abstract generic ancestor class for queries parametrized for collection type
 /// [C] and result type [R].
 ///
-/// Since queries may have specific implementations for particular container
-/// types, they are generics parametrized for container type.
+/// Since queries may have specific implementations for particular collection
+/// types, they are generics parametrized for collection type.
 abstract class Query<C extends GpsPointsView, R extends QueryResult> {
   /// Function that executes the actual query and returns the result.
-  R query(C container);
+  R query(C collection);
 }
 
 /// Query result for [QueryListInfo].
 class ListInfo extends QueryResult {
-  /// The start time of the first item in the container, if non-empty.
+  /// The start time of the first item in the collection, if non-empty.
   GpsTime? firstItemStartTime;
 
-  /// The end time of the last item in the container, if non-empty.
+  /// The end time of the last item in the collection, if non-empty.
   GpsTime? lastItemEndTime;
 
-  /// The number of items in the container.
+  /// The number of items in the collection.
   int length;
 
   ListInfo(this.firstItemStartTime, this.lastItemEndTime, this.length);
 }
 
-/// Queries generic information about the container and returns it as result
+/// Queries generic information about the collection and returns it as result
 /// of [ListInfo] type.
 class QueryListInfo<C extends GpsPointsView> extends Query<C, ListInfo> {
   @override
-  ListInfo query(C container) {
-    return ListInfo(container.isNotEmpty ? container.first.time : null,
-        container.isNotEmpty ? container.last.endTime : null, container.length);
+  ListInfo query(C collection) {
+    return ListInfo(
+        collection.isNotEmpty ? collection.first.time : null,
+        collection.isNotEmpty ? collection.last.endTime : null,
+        collection.length);
+  }
+}
+
+/// Query result for [QueryListItems].
+class SubList<C extends GpsPointsView> extends QueryResult {
+  final int startIndex;
+
+  final C collection;
+
+  SubList(this.startIndex, this.collection);
+}
+
+/// Returns specified item range from the collection and as result
+/// of [SubList] type.
+///
+/// The result will be a copy of the data, so that it can be transferred
+/// cheaply via an isolate port.
+class QueryListItems<C extends GpsPointsView> extends Query<C, SubList<C>> {
+  final int _startIndex;
+  final int? _nrItems;
+
+  /// Constructor for the query, with arguments indicating how much of the
+  /// collection should be returned.
+  ///
+  /// [startIndex] indicates, if provided, the first item that should be copied
+  /// by the query. If not provided, it defaults to 0.
+  ///
+  /// [nrItems] indicates, if provided, how many items should be copied by the
+  /// query. If not provided (or null), the copying will copy till the end of
+  /// the collection (equivalent to providing (length-startIndex)).
+  QueryListItems({int startIndex = 0, int? nrItems})
+      : _startIndex = startIndex,
+        _nrItems = nrItems;
+
+  @override
+  SubList<C> query(C collection) {
+    return SubList<C>(_startIndex,
+        collection.subList(startIndex: _startIndex, nrItems: _nrItems) as C);
   }
 }
 
