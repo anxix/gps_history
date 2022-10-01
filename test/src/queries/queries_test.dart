2a26ac0eb305faf026f37723711e13ee2a279bc4
anxix <51137713+anxix@users.noreply.github.com>
2022-10-01 12:26:09 +0200

    Added unit tests for QueryCollectionItems

--- test/src/queries/queries_test.dart
+++ test/src/queries/queries_test.dart
@@ -30,4 +30,66 @@ void main() {
       expect(queryResult.length, gpc.length);
     });
   });
+
+  checkEqualCollections(GpsPointsView actual, GpsPointsView expected) {
+    expect(actual.runtimeType, expected.runtimeType,
+        reason: 'Collections not of same type');
+
+    expect(actual.length, expected.length,
+        reason: 'Collection not of same length');
+
+    for (var i = 0; i < actual.length; i++) {
+      expect(actual[i], expected[i], reason: 'Incorrect item at position $i');
+    }
+  }
+
+  group('QueryCollectionItems', () {
+    void runTest<C extends GpsPointsView>(
+        C source, QueryCollectionItems<C> query, CollectionItems expected) {
+      final result = query.query(source);
+
+      expect(result.startIndex, expected.startIndex,
+          reason: 'Incorrect startIndex');
+
+      checkEqualCollections(result.collection, expected.collection);
+    }
+
+    test('Empty', () {
+      // Try on some type of collection.
+      final gpc = GpcListBased();
+      final query = QueryCollectionItems<GpcListBased>();
+      runTest(gpc, query, CollectionItems(0, gpc));
+    });
+
+    test('Entire list', () {
+      // Try on some different type of collection.
+      final gpc = GpcCompactGpsPoint()
+        ..add(GpsPoint.allZero)
+        ..add(GpsPoint.allZero.copyWith(time: GpsTime(10)));
+      final query = QueryCollectionItems<GpcCompactGpsPoint>();
+      runTest(gpc, query, CollectionItems(0, gpc));
+    });
+
+    test('Non-empty start list, empty result', () {
+      // Try on some different type of collection.
+      final gpc = GpcCompactGpsPoint()
+        ..add(GpsPoint.allZero)
+        ..add(GpsPoint.allZero.copyWith(time: GpsTime(10)));
+      final query =
+          QueryCollectionItems<GpcCompactGpsPoint>(startIndex: 1, nrItems: 0);
+      runTest(gpc, query, CollectionItems(1, gpc.newEmpty()));
+    });
+
+    test('Sub list', () {
+      // Try on some different type of collection.
+      final gpc = GpcCompactGpsPoint()
+        ..add(GpsPoint.allZero)
+        ..add(GpsPoint.allZero.copyWith(time: GpsTime(10)))
+        ..add(GpsPoint.allZero.copyWith(time: GpsTime(20)))
+        ..add(GpsPoint.allZero.copyWith(time: GpsTime(30)));
+      final query =
+          QueryCollectionItems<GpcCompactGpsPoint>(startIndex: 1, nrItems: 2);
+      runTest(gpc, query, CollectionItems(1, gpc.sublist(1, 3)));
+    });
+  });
 }
